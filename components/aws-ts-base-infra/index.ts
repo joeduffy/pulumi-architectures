5440d5cc52952a29796c38b41ef741ca7cb34ec8
Joe Duffy <joe@pulumi.com>
2020-05-02 11:12:50 -0700

    Fix dual mode exports

--- components/aws-ts-base-infra/index.ts
+++ components/aws-ts-base-infra/index.ts
@@ -1,15 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as co from "./component";
+import { Network } from "./component";
 import * as config from "./config";
 import * as utils from "./utils";
 
+// Always export the component so that typings get generated.
+export * from "./component";
+
+// If we're running this as a program rather than a library, create the network.
 if (utils.isPulumiMain(module)) {
     module.exports = async function() {
         // Fetch the configuration for this stack.
         const args = await config.getNetworkArgs();
 
         // If we're using the component as a project directly, simply instantiate it using config.
-        const network = new co.Network(`${pulumi.getProject()}-${pulumi.getStack()}`, args);
+        const network = new Network(`${pulumi.getProject()}-${pulumi.getStack()}`, args);
 
         // Export all of the resulting network properties that upstream stacks may want to consume.
         return {
@@ -25,6 +29,4 @@ if (utils.isPulumiMain(module)) {
             s3VpcEndpointId: network.s3VpcEndpointId,
         };
     }
-} else {
-    module.exports = co;
 }
